fa129a16947334fff8a88039fdca9abe8bd8285e
Pratyush Hota <pratyushhota1999@gmail.com>
2020-09-19 18:36:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,14 +6,13 @@ var sub = document.getElementById("s1");
 sub.addEventListener("click", fullName);
 
 function fullName() {
+    console.log("clicked...");
     let lname = document.getElementById("t3").value;
     let mname = document.getElementById("t2").value;
     let fname = document.getElementById("t1").value;
-    console.log(fname);
-    console.log(mname);
-    console.log(lname);
+   
 
-    console.log("clicked...");
+   
     
 
     document.getElementById('t4').innerHTML=fname+' '+mname+' '+lname;
