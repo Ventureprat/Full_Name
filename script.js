fa9b5f604a5665b4f1dd14b117e4331c433e9443
Pratyush Hota <pratyushhota1999@gmail.com>
2020-09-22 11:11:35 +0530

    added a clear button and removed password field

--- script.js
+++ script.js
@@ -10,14 +10,35 @@ function fullName() {
     let lname = document.getElementById("t3").value;
     let mname = document.getElementById("t2").value;
     let fname = document.getElementById("t1").value;
-   
-
-   
     
 
-    document.getElementById('t4').innerHTML=fname+' '+mname+' '+lname;
-    console.log(document.getElementById('t4').value);
+    
+    
+
+    document.getElementById("t4").innerHTML=fname+' '+mname+' '+lname;
+    
+     console.log(document.getElementById("t4").value);
 
     
 
 }
+
+var clic= document.getElementsByClassName(".c1");
+
+
+
+function cl() {
+     console.log("ClearAl function...");
+    
+
+    if(confirm("Are You Sure?")){
+     
+     document.getElementById("t1").value=" ";
+     document.getElementById("t2").value=" ";
+     document.getElementById("t3").value=" ";
+     document.getElementById("t4").value=" ";
+     
+    }
+     
+}
+
